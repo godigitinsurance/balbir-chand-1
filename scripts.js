fcf215726349fa6a87dfc8168fb17a48bc8ee946
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-26 10:15:57 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Balbir Chand");
+$("#policyNumber").text("LM96219085");
+$("#startDate").text("27-May-2020");
+$("#endDate").text("26-May-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CJ0931");
+$("#make").text("BAJAJ");
+$("#model").text("DISCOVER T");
+$("#vehicalSubType").text("KICK START (100 CC)");
+$("#engineNo").text("PAZPDL07979");
+$("#chassis").text("MD2A51BZ1DPL57590");
